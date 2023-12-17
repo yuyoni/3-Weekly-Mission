311781572bd44b35586deab45aaa3d18e7b759e3
yuyoni <doli0116@naver.com>
2023-12-17 11:42:31 +0900

    Feat : 비밀번호 유효성 검사 로직 추가

--- scripts/singin.js
+++ scripts/singin.js
@@ -12,18 +12,35 @@ const toggleIcon = (element, password) => {
 eyeIcons[0].addEventListener("click", () => toggleIcon(eyeIcons[0], pwdInput));
 
 /* 이메일 유효성 검사 */
-
 const isValidEmail = (element) => {
   const isEmpty = element.value.length === 0;
-  const validPattern = !/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(element.value);
+  const isInvalidPattern = !/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(
+    element.value
+  );
 
-  element.classList.toggle("invalid-border", isEmpty || validPattern);
+  element.classList.toggle("invalid-border", isEmpty || isInvalidPattern);
 
   errorMsgs[0].textContent = isEmpty
     ? "이메일을 입력해주세요."
-    : validPattern
+    : isInvalidPattern
     ? "올바른 이메일 주소가 아닙니다."
     : "";
 };
 
+/* 비밀번호 유효성 검사 */
+const isValidPwd = (element) => {
+  const isEmpty = element.value.length === 0;
+  const isInvalidPattern =
+    !/(?=.*[a-zA-Z])(?=.*\d).*/.test(element.value) || element.value.length < 8;
+
+  element.classList.toggle("invalid-border", isEmpty || isInvalidPattern);
+
+  errorMsgs[1].textContent = isEmpty
+    ? "비밀번호를 입력해주세요."
+    : isInvalidPattern
+    ? "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    : "";
+};
+
 emailInput.addEventListener("focusout", () => isValidEmail(emailInput));
+pwdInput.addEventListener("focusout", () => isValidPwd(pwdInput));
