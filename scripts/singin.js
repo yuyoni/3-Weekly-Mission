eab925801fa19e44955567f8894dd2d38b67c871
yuyoni <doli0116@naver.com>
2023-12-17 11:55:59 +0900

    Feat : test@codeit.com으로 로그인 시도 시 페이지 이동 or 오류메세지 나타내는 로직 추가

--- scripts/singin.js
+++ scripts/singin.js
@@ -2,6 +2,7 @@ const emailInput = document.querySelector("#email");
 const pwdInput = document.querySelector("#password");
 const eyeIcons = document.querySelectorAll(".eye-icon");
 const errorMsgs = document.querySelectorAll(".error-message");
+const loginForm = document.querySelector("#login-form");
 
 const toggleIcon = (element, password) => {
   password.type = password.type === "password" ? "text" : "password";
@@ -44,3 +45,20 @@ const isValidPwd = (element) => {
 
 emailInput.addEventListener("focusout", () => isValidEmail(emailInput));
 pwdInput.addEventListener("focusout", () => isValidPwd(pwdInput));
+
+/* "test@codeit.com" 이메일과 "codeit101" 비밀번호로 로그인하면 이동, 틀리면 오류메세지 */
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const rightEmail = "test@codeit.com";
+  const rightPwd = "codeit101";
+
+  if (emailInput.value === rightEmail && pwdInput.value === rightPwd) {
+    window.location.href = "folder.html";
+  } else {
+    emailInput.classList.add("invalid-border");
+    pwdInput.classList.add("invalid-border");
+    errorMsgs[0].textContent = "이메일을 확인해주세요.";
+    errorMsgs[1].textContent = "비밀번호를 확인해주세요.";
+  }
+});
