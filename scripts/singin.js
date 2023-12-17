e483a6ab6756720ad99351f414eee5d1544c1bce
yuyoni <doli0116@naver.com>
2023-12-17 11:27:18 +0900

    Feat : 이메일 유효성 검사 로직 추가 및 스타일 변경

--- scripts/singin.js
+++ scripts/singin.js
@@ -1,5 +1,7 @@
+const emailInput = document.querySelector("#email");
 const pwdInput = document.querySelector("#password");
 const eyeIcons = document.querySelectorAll(".eye-icon");
+const errorMsgs = document.querySelectorAll(".error-message");
 
 const toggleIcon = (element, password) => {
   password.type = password.type === "password" ? "text" : "password";
@@ -8,3 +10,20 @@ const toggleIcon = (element, password) => {
 };
 
 eyeIcons[0].addEventListener("click", () => toggleIcon(eyeIcons[0], pwdInput));
+
+/* 이메일 유효성 검사 */
+
+const isValidEmail = (element) => {
+  const isEmpty = element.value.length === 0;
+  const validPattern = !/^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/.test(element.value);
+
+  element.classList.toggle("invalid-border", isEmpty || validPattern);
+
+  errorMsgs[0].textContent = isEmpty
+    ? "이메일을 입력해주세요."
+    : validPattern
+    ? "올바른 이메일 주소가 아닙니다."
+    : "";
+};
+
+emailInput.addEventListener("focusout", () => isValidEmail(emailInput));
