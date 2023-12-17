68be70c573389accb14f0cf752d139183a2ae15b
yuyoni <doli0116@naver.com>
2023-12-17 12:55:38 +0900

    Feat : 유효한 회원가입 시도 시 페이지 이동

--- scripts/singup.js
+++ scripts/singup.js
@@ -20,12 +20,18 @@ pwdInputs.forEach((el, idx) =>
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (emailInput.value === "test@codeit.com") {
-    emailInput.classList.toggle(
-      "invalid-border",
-      emailInput.value === "test@codeit.com"
-    );
+
+  const isExistEmail = emailInput.value === "test@codeit.com";
+  const isEmptyInput =
+    emailInput.value.length === 0 || pwdInputs[0].value.length === 0;
+  const isEmptyError = [...errorMsgs].some((el) => el.textContent.length > 0);
+
+  /* "test@codeit.com"로 회원가입 시도 시 오류 메세지 */
+  if (isExistEmail) {
+    emailInput.classList.toggle("invalid-border", isExistEmail);
     errorMsgs[0].textContent = "이미 사용중인 이메일 입니다.";
-  } else {
+  } else if (!isEmptyInput && !isEmptyError) {
+    /* input이 비어있지 않으면서 오류 메시지가 없으면 회원가입 성공 */
+    window.location.href = "folder.html";
   }
 });
