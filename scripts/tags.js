221c0c72d93dfb2d421ce033859da598bd126092
yuyoni <doli0116@naver.com>
2023-12-24 21:38:32 +0900

    Chore : form태그와 input태그 id를 class로 수정

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,12 +1,12 @@
-const emailInput = document.querySelector("#email");
+const emailInput = document.querySelector(".email");
 const pwdInput = document.querySelectorAll(".password")[0];
 const pwdCheckInput = document.querySelectorAll(".password")[1];
 const eyeIcons = document.querySelectorAll(".eye-icon");
 const emailErrorMsg = document.querySelectorAll(".error-message")[0];
 const pwdErrorMsg = document.querySelectorAll(".error-message")[1];
 const pwdCheckErrorMsg = document.querySelectorAll(".error-message")[2];
-const loginForm = document.querySelector("#login-form");
-const signupForm = document.querySelector("#signup-form");
+const loginForm = document.querySelector(".login-form");
+const signupForm = document.querySelector(".signup-form");
 
 export {
   emailInput,
