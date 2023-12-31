d2f4ce7ab3be306755ca5714057bf0db93e7dc14
yuyoni <doli0116@naver.com>
2023-12-31 15:39:24 +0900

    Refactor : api 요청 예외 처리 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,23 @@
 export async function fetchFolderData() {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folder"
-  );
-  const body = await response.json();
-  return body;
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/folder"
+    );
+    const body = await response.json();
+    return body;
+  } catch {
+    throw Error("폴더 정보를 찾을 수 없습니다.");
+  }
 }
 
 export async function fetchUserData() {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/user"
-  );
-  const body = await response.json();
-  return body;
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/user"
+    );
+    const body = await response.json();
+    return body;
+  } catch {
+    throw Error("사용자 정보를 찾을 수 없습니다. ");
+  }
 }
