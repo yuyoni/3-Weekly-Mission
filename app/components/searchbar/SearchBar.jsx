40a05c6a0a711e590ab1e08a4223ed261ea488fa
yuyoni <doli0116@naver.com>
2024-02-26 17:37:36 +0900

    Feat : SearchBar에 입력한 text값 저장 및 text 초기화하는 버튼 생성

--- app/components/searchbar/SearchBar.jsx
+++ app/components/searchbar/SearchBar.jsx
@@ -1,12 +1,32 @@
 import styled from "styled-components";
 import search from "../../../public/assets/Search.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function SearchBar() {
+  const [text, setText] = useState("");
+
+  const onChangeInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    setText("");
+  };
+
+  useEffect(() => {
+    // 현재 폴더의 링크 카드 중 “url”, “title”, “description”에 text가 들어가는 카드만 포함된 링크들만 필터링하는 기능
+  }, [text]);
+
   return (
     <Wrapper>
       <Image src={search} alt="search-icon" />
-      <input placeholder=" 링크를 검색해 보세요" />
+      <input
+        value={text}
+        onChange={onChangeInput}
+        placeholder=" 링크를 검색해 보세요"
+      />
+      <button onClick={onClickButton}>x</button>
     </Wrapper>
   );
 }
