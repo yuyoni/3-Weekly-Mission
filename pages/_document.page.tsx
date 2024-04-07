514b2a58708717a1ee7aab1e1acb9854ac708879
YU YEON LEE <doli0116@naver.com>
2024-04-08 01:26:54 +0900

    Refactor: 메타태그 수정

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -3,17 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>Linkbrary</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:url" content="https://codeitlinkbrary.netlify.app" />
-        <meta property="og:image" content="images/description.png" />
-        <meta property="og:title" content="Linkbrary" />
-        <meta
-          property="og:description"
-          content="세상의 모든 정보를 쉽게 저장하고 관리해보세요"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
 import Layout from "@components/common/Layout";
 import styles from "./styles/Home.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Linkbrary</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:url"
+          content="https://3-weekly-mission-uefr.vercel.app/"
+        />
+        <meta property="og:image" content="images/description.png" />
+        <meta property="og:title" content="Linkbrary" />
+        <meta
+          property="og:description"
+          content="세상의 모든 정보를 쉽게 저장하고 관리해보세요"
+        />
+      </Head>
       <div className={styles.container}>
         <h1>Home</h1>
         <Link href={"/folder"} legacyBehavior>
