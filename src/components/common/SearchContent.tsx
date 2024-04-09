27bb2977d1c183c53dcabc880a2caf9354af0669
YU YEON LEE <doli0116@naver.com>
2024-04-09 10:49:15 +0900

    Refactor: 변경 사항이 있을 때 link 상태 업데이트 하도록 수정

--- src/components/common/SearchContent.tsx
+++ src/components/common/SearchContent.tsx
@@ -1,7 +1,7 @@
 import Card from "@components/card/Card";
 import filterLinksBySearchText from "@utils/filterLinksBySearchText";
 import handleDragAndDrop from "@utils/handleDragandDrop";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FolderData, LinkList } from "type";
 import styles from "./SearchContent.module.css";
 import SearchContentSkeleton from "./SearchContentSkeleton";
@@ -19,6 +19,19 @@ export default function SearchContent({
   links,
   folders,
 }: SearchContentProps) {
+  const [arrangedLinks, setArrangedLinks] = useState<LinkList[] | undefined>();
+
+  useEffect(() => {
+    if (links && links.length > 0) {
+      const filteredLinks = links.filter((link: LinkList) =>
+        filterLinksBySearchText(link, inputText)
+      );
+      setArrangedLinks(filteredLinks);
+    } else {
+      setArrangedLinks([]);
+    }
+  }, [links, inputText]);
+
   if (isLinkPending)
     return (
       <div className={styles.wrapper}>
@@ -26,42 +39,38 @@ export default function SearchContent({
       </div>
     );
 
-  const filteredLinks =
-    links?.filter((link: LinkList) =>
-      filterLinksBySearchText(link, inputText)
-    ) ?? [];
-  const [arrangedLinks, setArrangedLinks] = useState(filteredLinks);
-
-  return arrangedLinks.length > 0 ? (
+  return (
     <div className={styles.wrapper}>
-      {arrangedLinks.map((link, index) => (
-        <div
-          key={link.id}
-          draggable
-          onDragStart={(event: React.DragEvent<HTMLDivElement>) => {
-            event.dataTransfer.effectAllowed = "move";
-            event.dataTransfer.setData("index", index.toString());
-          }}
-          onDragOver={(event: React.DragEvent<HTMLDivElement>) => {
-            event.preventDefault();
-          }}
-          onDrop={(event: React.DragEvent<HTMLDivElement>) => {
-            const fromIndex = Number(event.dataTransfer.getData("index"));
-            const toIndex = index;
-            handleDragAndDrop<LinkList>(
-              event,
-              fromIndex,
-              toIndex,
-              arrangedLinks,
-              setArrangedLinks
-            );
-          }}
-        >
-          <Card key={link.id} folders={folders} link={link} />
-        </div>
-      ))}
+      {arrangedLinks && arrangedLinks.length > 0 ? (
+        arrangedLinks.map((link, index) => (
+          <div
+            key={link.id}
+            draggable
+            onDragStart={(event: React.DragEvent<HTMLDivElement>) => {
+              event.dataTransfer.effectAllowed = "move";
+              event.dataTransfer.setData("index", index.toString());
+            }}
+            onDragOver={(event: React.DragEvent<HTMLDivElement>) => {
+              event.preventDefault();
+            }}
+            onDrop={(event: React.DragEvent<HTMLDivElement>) => {
+              const fromIndex = Number(event.dataTransfer.getData("index"));
+              const toIndex = index;
+              handleDragAndDrop<LinkList>(
+                event,
+                fromIndex,
+                toIndex,
+                arrangedLinks,
+                setArrangedLinks
+              );
+            }}
+          >
+            <Card key={link.id} folders={folders} link={link} />
+          </div>
+        ))
+      ) : (
+        <p className={styles.no_link}>저장된 링크가 없습니다</p>
+      )}
     </div>
-  ) : (
-    <p className={styles.no_link}>저장된 링크가 없습니다</p>
   );
 }
